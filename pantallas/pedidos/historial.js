3e3de3ef04c52e958b1ea4dfe30c5fcdff0805ad
Elmerrf55 <20220240@ricaldone.edu.sv>
2024-07-09 23:14:18 -0600

    detalle pedidos ya esta

--- pantallas/pedidos/historial.js
+++ pantallas/pedidos/historial.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import OrderList from './pantallas/components/lista_pedido';
 import Header from './pantallas/components/encabezado';
+import OrderDetail from './pantallas/components/OrderDetail';
+
+const Stack = createStackNavigator();
+
 const Historial = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <Header />
-            <OrderList />
-        </SafeAreaView>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="OrderList" component={OrderList} options={{ title: 'Historial' }} />
+                <Stack.Screen name="OrderDetail" component={OrderDetail} options={{ title: 'Detalle del Pedido' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
 
 export default Historial;
