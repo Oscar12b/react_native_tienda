37cab504845a0b117e9dbd4cda98146ebdb75525
Elmerrf55 <20220240@ricaldone.edu.sv>
2024-08-22 07:52:00 -0600

    modificaciones

--- pantallas/componentes/lista_pedido.js
+++ pantallas/componentes/lista_pedido.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import SearchBar from './pantallas/components/barra_busqueda';
-import OrderItem from './pantallas/components/articulo';
+import React from 'react';  // Importa el módulo React para usar sus funcionalidades.
+import { View, StyleSheet, ScrollView } from 'react-native'; // Importa componentes necesarios de React Native.
+import SearchBar from './pantallas/components/barra_busqueda'; // Importa el componente SearchBar desde la ruta especificada.
+import OrderItem from './pantallas/components/articulo'; // Importa el componente OrderItem desde la ruta especificada.
 
-const OrderList = () => {
+const OrderList = () => { // Define el componente funcional OrderList.
+    // Array de pedidos con ID y fecha para cada pedido.
     const orders = [
         { id: 1, date: '30/05/2024' },
         { id: 2, date: '30/06/2024' },
@@ -14,24 +15,27 @@ const OrderList = () => {
 
     return (
         <View style={styles.container}>
-            <SearchBar />
+            <SearchBar /> 
             <ScrollView contentContainerStyle={styles.scrollContainer}>
+                
                 {orders.map(order => (
-                    <OrderItem key={order.id} order={order} />
+                    <OrderItem key={order.id} order={order} /> /* Renderiza un OrderItem para cada pedido, usando el ID del pedido como clave única. */
                 ))}
             </ScrollView>
         </View>
     );
 };
 
+// Define los estilos utilizados en el componente OrderList.
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 20,
+        flex: 1, // Hace que el contenedor ocupe todo el espacio disponible.
+        padding: 20, // Agrega un relleno de 20 unidades alrededor del contenido del contenedor.
     },
     scrollContainer: {
-        paddingVertical: 20,
+        paddingVertical: 20, // Agrega relleno vertical de 20 unidades dentro del ScrollView.
     },
 });
 
-export default OrderList;
+export default OrderList; // Exporta el componente OrderList para que pueda ser utilizado en otras partes de la aplicación.
+
